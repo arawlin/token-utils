950d017a0ec7707bc0e2443ba9723b5682923522
linsilence <linlinsilence@gmail.com>
2022-05-25 16:15:55 +0800

    task, balances 4

--- libs/etherUtils.js
+++ libs/etherUtils.js
@@ -4,20 +4,29 @@ const path = require('path')
 const loadWallets = async (ethers, dir, pass, deal) => {
   console.log('loadWallets --------- ', dir)
 
+  let idx = 0
   const ws = []
   const files = await fs.readdir(dir)
   for (const fn of files) {
     // if (!fn.endsWith('.keystore')) {
     //   continue
     // }
+
+    ++idx
     try {
       const ct = await fs.readFile(path.join(dir, fn), { encoding: 'utf-8' })
 
       let w = await ethers.Wallet.fromEncryptedJson(ct, pass)
       w = w.connect(ethers.provider)
-      deal && (await deal(w))
 
-      ws.push(w)
+      let include = true
+      if (deal) {
+        include = await deal(w, idx, fn)
+      }
+
+      if (include) {
+        ws.push(w)
+      }
     } catch (e) {
       console.error(e)
     }
@@ -28,6 +37,22 @@ const loadWallets = async (ethers, dir, pass, deal) => {
 
 const loadWalletsBalance = async (ethers, dir, pass, over = 0) => {
   console.log('loadWalletsBalance --------- ')
+
+  let total = ethers.BigNumber.from('0')
+  const wss = await loadWallets(ethers, dir, pass, async (w, i, f) => {
+    const b = await logBalance(ethers, w, `${i} - ${f} -`)
+    const include = b.gte(ethers.BigNumber.from(over))
+    if (include) {
+      total = total.add(b)
+    }
+    return include
+  })
+  console.log(`loadWalletsBalance - count: ${wss.length}, total: ${ethers.utils.formatEther(total)}`)
+  return wss
+}
+
+const loadWalletsBalanceSyn = async (ethers, dir, pass, over = 0) => {
+  console.log('loadWalletsBalanceSyn --------- ')
 
   let total = ethers.BigNumber.from('0')
 
@@ -44,7 +69,7 @@ const loadWalletsBalance = async (ethers, dir, pass, over = 0) => {
     total = total.add(b)
     wss.push(w)
   }
-  console.log(`loadWalletsBalance - count: ${wss.length}, total: ${ethers.utils.formatEther(total)}`)
+  console.log(`loadWalletsBalanceSyn - count: ${wss.length}, total: ${ethers.utils.formatEther(total)}`)
   return wss
 }
 
@@ -57,5 +82,6 @@ const logBalance = async (ethers, signer, label = '') => {
 module.exports = {
   loadWallets,
   loadWalletsBalance,
+  loadWalletsBalanceSyn,
   logBalance,
 }
