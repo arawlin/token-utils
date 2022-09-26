d558bb7ed0665482f612e5c687410d238aae2d5a
abc <abc@gmail.com>
2022-09-26 17:53:27 +0800

    contract proxy, openzeppelin/hardhat-upgrades tool

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-solhint')
 // require('hardhat-gas-reporter')
 require('solidity-coverage')
+require('@openzeppelin/hardhat-upgrades')
 require('./plugins/hardhat-web3-ws')
 
 require('./db').connect()
