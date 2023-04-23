c1e429ad577c38568032f53cff715aeafea3e11a
abc <abc@gmail.com>
2023-04-23 15:54:33 +0800

    task, mevLike, 8, uniswapUtils 2

--- libs/uniswapUtils.js
+++ libs/uniswapUtils.js
@@ -6,25 +6,36 @@ const abiUniversalRouter = require('../abis/uniswap/UniversalRouter.json')
 const abiERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json').abi
 
 const DELAY_DEADLINE = 120
-const RATIO_SLIPPAGE = 0.1
+const RATIO_MAX = ethers.BigNumber.from(100)
+const RATIO_SLIPPAGE = ethers.BigNumber.from(10)
 
-const buyETH = async (addrToken, valueETH, addrSinger, gasPriceMultiply = 1) => {
-  const signer = await ethers.getSigner(addrSinger)
+const buyETH = async (addrToken, valueETH, gasPricePercent = RATIO_MAX) => {
+  const signer = (await ethers.getSigners())[0]
   const router = new ethers.Contract(process.env.addrUniswapV2Router02, abiUniswapV2Router02, signer)
 
+  const deadline = timeInterval(DELAY_DEADLINE)
+
+  // find the best path
   const path = [process.env.addrWETH, addrToken]
-  const dealline = timeInterval(DELAY_DEADLINE)
 
-  const gasPrice = 0
+  let gasPrice = await ethers.provider.getGasPrice()
+  gasPrice = gasPrice.mul(gasPricePercent)
 
-  const amountOutMin = 0
+  const amounts = await router.getAmountsOut(valueETH, path)
+  const amountOutMin = amounts[path.length - 1].mul(RATIO_MAX.sub(RATIO_SLIPPAGE)).div(RATIO_MAX)
 
-  const res = await router.swapExactETHForTokens(amountOutMin, path, addrSinger, dealline, { value: valueETH, gasPrice })
+  const res = await router.swapExactETHForTokens(amountOutMin, path, signer.address, deadline, { value: valueETH, gasPrice })
   const rec = await res.wait()
 
   // approve first
 }
 
+const sellETH = async (addrToken, valuePercent, gasPricePercent = RATIO_MAX) => {
+  //  balance enough
+  // decimals
+}
+
 module.exports = {
   buyETH,
+  sellETH,
 }
--- test/uniswapUtils.test.js
+++ test/uniswapUtils.test.js
@@ -1,19 +1,44 @@
 const { ethers } = require('hardhat')
 const uni = require('../libs/uniswapUtils')
 
+const abiUniswapV2Router02 = require('../abis/uniswap/UniswapV2Router02.json')
+const abiERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json').abi
+
 describe('uniswap', () => {
-  it('contract overrides', async () => {
-    const hash = '0x0a3b71b2eac59936a24190f3da839c658ef4fb6156109b2a86096a82cb041372'
+  it.skip('contract overrides', async () => {
+    const hash = '0xd7c0d2a5aa3b6697e03f8d6a0c16503d29d9059199e763500f21f1480b88bbd5'
     const tx = await ethers.provider.getTransaction(hash)
     console.log(tx)
+    console.log(tx.timestamp)
 
     const tr = await ethers.provider.getTransactionReceipt(hash)
     console.log(tr)
 
+    const block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber())
+    console.log(block)
+    console.log(ethers.utils.formatUnits(block.baseFeePerGas, 'gwei'), 'gwei')
+    console.log(block.timestamp, new Date(block.timestamp * 1000), new Date())
+
     // gasPrice
+    const gasPrice = await ethers.provider.getGasPrice()
+    console.log(gasPrice, ethers.utils.formatUnits(gasPrice, 'gwei'), 'gwei')
   })
 
-  it('buyETH', async () => {
-    // await uni.buyETH(process.env.addrUniswapper)
+  it('router info', async () => {
+    // usdt - 0xdAC17F958D2ee523a2206206994597C13D831ec7
+    const addrToken = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    const token = new ethers.Contract(addrToken, abiERC20, ethers.provider)
+    const symbol = await token.symbol()
+    const decimals = await token.decimals()
+    console.log(symbol, decimals)
+
+    const router = new ethers.Contract(process.env.addrUniswapV2Router02, abiUniswapV2Router02, ethers.provider)
+    const amountIn = ethers.utils.parseEther('1')
+    const path = [process.env.addrWETH, addrToken]
+    const amounts = await router.getAmountsOut(amountIn, path)
+    console.log(ethers.utils.commify(amounts[path.length - 1]))
+    console.log(ethers.utils.formatUnits(amounts[path.length - 1], decimals))
   })
+
+  it('uniswap sdk', async () => {})
 })
