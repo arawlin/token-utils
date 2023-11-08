5de67a232792b9cee0a42b165b27a171e97e5a5f
abc <abc@gmail.com>
2023-11-08 16:23:58 +0800

    task, accounts

--- tasks/accounts.js
+++ tasks/accounts.js
@@ -1,9 +1,12 @@
 const path = require('path')
+const fs = require('node:fs/promises')
 const { DIR_RES_KEYSTORES } = require('../libs/constants')
 const utils = require('../libs/etherUtils')
 
-const action = async ({ d, p, c, n }, { ethers }) => {
+const action = async ({ d, p, c, n, s }, { ethers }) => {
   const dir = path.join(DIR_RES_KEYSTORES, d || process.env.DIR_KEYSTORE)
+  await fs.mkdir(dir, { recursive: true })
+
   const pass = p || process.env.PASS_KEYSTORE
   if (!pass) {
     throw new Error('params error')
@@ -11,6 +14,11 @@ const action = async ({ d, p, c, n }, { ethers }) => {
 
   if (c) {
     await utils.createWallets(ethers, dir, pass, Number(n))
+  }
+  if (s) {
+    await utils.loadWallets(ethers, dir, pass, async (w, i, f) => {
+      console.log(`address: ${w.address}, ${w.publicKey} - ${w.privateKey} - ${i}`)
+    })
   }
 }
 
@@ -21,7 +29,8 @@ module.exports = {
     { name: 'd', description: 'keystore dir name', defaultValue: '' },
     { name: 'p', description: 'keystore password', defaultValue: '' },
     { name: 'c', description: 'create accounts', paramType: 'flag' },
-    { name: 'n', description: 'How many accounts need to be created' },
+    { name: 's', description: 'show accounts', paramType: 'flag' },
+    { name: 'n', description: 'How many accounts need to be created', defaultValue: '0' },
   ],
   action,
 }
